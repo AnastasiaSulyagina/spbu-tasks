51f520472e2ff6b1b95bb18657629ce73e8b3fe8
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2014-05-25 18:51:51 +0400

    Web crawler added

---
+++ web.fs
@@ -0,0 +1,36 @@
+﻿ //Sulyagina Anastasia
+//Web crawler
+module web
+
+open System
+open WebR
+
+let imgCount page =
+    let rec imgCount' (page : string) (pos : int) =
+        let x = page.IndexOf("<img", pos)
+        if x = -1
+        then 0
+        else 1 + imgCount' page (x + 1)
+    imgCount' page 0
+
+let rec getLink page =
+    let rec getLink' (page : string) (pos : int) =
+        if imgCount page < 5 then []
+        else
+            let count = 0
+            let x = page.IndexOf("<img", pos)
+            if x = -1 then []
+            else
+                let st = page.IndexOf("src=", x) + 5
+                let fin = page.IndexOf("\"", st)
+                page.Substring(st, fin - st) :: getLink' page (fin)
+    getLink' page 0
+
+let rec getImg l f =
+    match l with
+        | [] -> f []
+        | hd :: tl -> getUrl hd (fun x -> getImg tl (fun y -> f (Seq.toList (Seq.distinct ([getLink x] @ y )))))
+
+getImg [ "http://www.google.com/"; "http://www.4pda.ru/"] (printfn "%A")
+
+Console.ReadLine() |> ignore
