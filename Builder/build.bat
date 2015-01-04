c4bdcfb5c4107cfca9f08d172357767e777fd907
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2015-01-05 00:30:22 +0400

    Check if build files exist added

--- Builder/build.bat
+++ Builder/build.bat
@@ -3,3 +3,9 @@ if "%running%"=="" goto :EOF
 
 %MSBuildPath%\MSBuild.exe %RepositoryPath%\%RepositoryName%\%solution% /p:Configuration=Release;VisualStudioVersion=13.0 > %BuildLog%
 if ERRORLEVEL 1 set BuildSucceeded=false
+
+for /F %%file in %FileList% do (
+	if not exist %BuildLocation%\%%file (
+		set BuildSucceeded=false
+	)
+) 
--- Builder/settings.bat
+++ Builder/settings.bat
@@ -12,7 +12,7 @@ set GitPath="C:\Program Files (x86)\Git\bin"
 set MSBuildPath="C:\Windows\Microsoft.NET\Framework\v4.0.30319"
 set BlatPath="C:\Program Files (x86)\blat-2.6.2\full\"
 set NUnitPath="C:\Program Files (x86)\NUnit-2.6.4\bin\"
-set FileList=%folder%\files.txt
+set FileList=files.txt
 
 set RepositoryPath="C:\GitHub"
 set RepositoryName=Geometry_For_Builder
