d6adf81497b6e83afdeaae9c3ef45a57f669a8a4
Anastasia Sulyagina <anastasia.sulyagina@gmail.com>
2015-05-02 00:21:23 +0300

    Database task added

---
+++ Airport/Airport/Client.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [Phone] NCHAR(10) NOT NULL, 
+    [CardNumber] NVARCHAR(MAX) NOT NULL, 
+    [PassportNumber] NVARCHAR(MAX) NOT NULL
+		
+)
---
+++ Airport/Airport/Flight.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Flights]
+(
+	[Number] INT NOT NULL PRIMARY KEY, 
+    [DateTime] DATETIME NOT NULL, 
+    [DestinationPort] NVARCHAR(50) NOT NULL, 
+    [Plane] NVARCHAR(50) NOT NULL, 
+    [PlacesLeft] INT NOT NULL, 
+    [DeparturePort] NVARCHAR(50) NOT NULL
+	CONSTRAINT [FK_Flights_Planes] FOREIGN KEY ([Plane]) REFERENCES [Planes]([Number])
+)
